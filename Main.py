7af1ce4f8809b43f6b820a4ab88b6b4856168a44
Albert <97989034+albertpang@users.noreply.github.com>
2023-05-26 11:13:52 -0400

    Working print

--- Main.py
+++ Main.py
@@ -1,13 +1,16 @@
 import time
 import imaplib
 import email as em
+from escpos.printer import Usb
 from email.header import decode_header
 import re
 
 # IMAP stands for Internet Message Access Protocol
+
 class GmailIMAP():
-    def __init__(self) -> None:
+    def __init__(self, printer) -> None:
         '''Control the process of login, pulling, parsing from Gmail'''
+        self.printer = printer
         self.set_gmail_connection()
         self.read_email_inbox()
 
@@ -20,12 +23,12 @@ class GmailIMAP():
         username = loginFile.readline().strip()
         password = loginFile.readline()
         return(username, password)
-    
+
     def set_gmail_connection(self):
         '''Set up IMAP with gmail account'''
         self.imap = imaplib.IMAP4_SSL("imap.gmail.com")
         user, pw = self.get_login()
-        self.imap.login(user, pw)      
+        self.imap.login(user, pw)
 
     def read_email_inbox(self):
         '''Reads all emails found in the email inbox'''
@@ -46,13 +49,13 @@ class GmailIMAP():
 
     def pull_metadata(self, email):
         '''Takes the Email object and pulls metadata using decode_header method'''
-        title = decode_header(email["Subject"])[0]        
+        title = decode_header(email["Subject"])[0]
         receiveTime = decode_header(email["Date"])[0]
         sender = decode_header(email['From'])[0]
-        
-        print(f'Subject: {title[0]}')
-        print(f'Received: {receiveTime[0]}')
-        print(f'From: {sender[0]}')
+
+        self.printer.text(f'Subject: {title[0]}\n')
+        self.printer.text(f'Received: {receiveTime[0]}\n')
+        self.printer.text(f'From: {sender[0]}\n')
 
         # Retrieve and print the email body
         if email.is_multipart():
@@ -60,12 +63,12 @@ class GmailIMAP():
                 content_type = part.get_content_type()
                 if content_type == "text/plain":
                     body = part.get_payload(decode=True).decode()
-                    print(f"Body:{body}")
+                    self.printer.text(f"Body:{body}\n")
         else:
             body = email.get_payload(decode=True)
             decodedBody = body.decode()
-            print(f"Body: {decodedBody}")
-        print("="*70)
+            self.printer.text(f"Body: {decodedBody}\n")
+        self.printer.text("="*10)
 
     def move_folder(self, email_id):
         '''Moves the email to the Printed label and removes the Inbox label'''
@@ -78,7 +81,7 @@ class GmailIMAP():
                 return None
             match = pattern_uid.match(data.decode('utf-8'))
             return match.group('uid')
-        
+
         # Loop through the data and retrieve the corresponding emails
         uid_data = self.imap.fetch(email_id, "(UID)")[1]
         uid = parse_uid(uid_data[0])
@@ -89,10 +92,11 @@ class GmailIMAP():
 
 
 if __name__ == "__main__":
-    print("==================")
-    print(" THERMAL PRINTER ")
-    print("==================")
-    
+    printer = Usb(0x0416, 0x5011, in_ep=0x81, out_ep=0x01, profile="POS-5890")
+    printer.text("==================\n")
+    printer.text(" THERMAL PRINTER \n")
+    printer.text("==================\n")
+
     while True:
-        gm = GmailIMAP()
-        time.sleep(120)
+        gm = GmailIMAP(printer)
+        time.sleep(20)
