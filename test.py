1d365cc2340f77a38b94d62196dcc810c99f1674
Albert <97989034+albertpang@users.noreply.github.com>
2025-10-22 10:17:31 -0700

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from escpos
-from escpos.printer import Usb
-
-""" Seiko Epson Corp. Receipt Printer (EPSON TM-T88III) """
-p = Usb(0x02b8, 0x0202, 0, profile="TM-T88III")
-p.text("Hello World\n")
-p.image("logo.gif")
-p.barcode('1324354657687', 'EAN13', 64, 2, '', '')
-p.cut()
